7039539c578c3d6b724873d4f7a4ec671286153b
Lorenzo Vagliano <111889654+Lorenzovagliano@users.noreply.github.com>
2023-09-12 12:54:56 -0300

    ordenarPolar

--- teste.cpp
+++ teste.cpp
@@ -1,131 +1,89 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
-#include <algorithm>
 
-struct Point {
-    int64_t x, y;
+class Ponto{
+    public:
+        double x, y;
 
-    Point(int64_t x_, int64_t y_): x(x_), y(y_) {}
+        Ponto(){
+            this->x = 0;
+            this->y = 0;
+        }
 
-    Point operator - (const Point & p) const {
-        return Point(x - p.x, y - p.y);
-    }
-
-    int64_t cross (const Point & p) const {
-        return x * p.y - y * p.x;
-    }
-
-    int64_t cross (const Point & p, const Point & q) const {
-        return (p - *this).cross(q - *this);
-    }
-
-    int half () const {
-        return int(y < 0 || (y == 0 && x < 0));
-    }
+        Ponto(int _x, int _y){
+            this->x = _x;
+            this->y = _y;
+        }
 };
 
-std::vector<std::vector<size_t>> find_faces(std::vector<Point> vertices, std::vector<std::vector<size_t>> adj) {
-    size_t n = vertices.size();
-    std::vector<std::vector<char>> used(n);
-    for (size_t i = 0; i < n; i++) {
-        used[i].resize(adj[i].size());
-        used[i].assign(adj[i].size(), 0);
-        auto compare = [&](size_t l, size_t r) {
-            Point pl = vertices[l] - vertices[i];
-            Point pr = vertices[r] - vertices[i];
-            if (pl.half() != pr.half())
-                return pl.half() < pr.half();
-            return pl.cross(pr) > 0;
-        };
-        std::sort(adj[i].begin(), adj[i].end(), compare);
-    }
-    std::vector<std::vector<size_t>> faces;
-    for (size_t i = 0; i < n; i++) {
-        for (size_t edge_id = 0; edge_id < adj[i].size(); edge_id++) {
-            if (used[i][edge_id]) {
-                continue;
-            }
-            std::vector<size_t> face;
-            size_t v = i;
-            size_t e = edge_id;
-            while (!used[v][e]) {
-                used[v][e] = true;
-                face.push_back(v);
-                size_t u = adj[v][e];
-                size_t e1 = std::lower_bound(adj[u].begin(), adj[u].end(), v, [&](size_t l, size_t r) {
-                    Point pl = vertices[l] - vertices[u];
-                    Point pr = vertices[r] - vertices[u];
-                    if (pl.half() != pr.half())
-                        return pl.half() < pr.half();
-                    return pl.cross(pr) > 0;
-                }) - adj[u].begin() + 1;
-                if (e1 == adj[u].size()) {
-                    e1 = 0;
-                }
-                v = u;
-                e = e1;
-            }
-            std::reverse(face.begin(), face.end());
-            int sign = 0;
-            for (size_t j = 0; j < face.size(); j++) {
-                size_t j1 = (j + 1) % face.size();
-                size_t j2 = (j + 2) % face.size();
-                int64_t val = vertices[face[j]].cross(vertices[face[j1]], vertices[face[j2]]);
-                if (val > 0) {
-                    sign = 1;
-                    break;
-                } else if (val < 0) {
-                    sign = -1;
-                    break;
-                }
-            }
-            if (sign <= 0) {
-                faces.insert(faces.begin(), face);
-            } else {
-                faces.emplace_back(face);
-            }
+class Vertice{
+    public:
+        int id;
+        Ponto cord;
+        int grau;
+        std::vector<Vertice> lista_adj;
+
+        Vertice(){
+            this->id = 0;
+            this->cord = Ponto();
+            this->grau = 0;
         }
-    }
-    return faces;
+
+        Vertice(int _id, int _grau, int _x, int _y){
+            this->id = _id;
+            this->cord = Ponto(_x, _y);
+            this->grau = _grau;
+        }
+};
+
+// Dist^ancia euclidiana de a para b.
+double distancia(Ponto a, Ponto b) {
+    double x = (a.x - b.x), y = (a.y - b.y);
+    return sqrt(x*x + y*y);
 }
 
-int main() {
-    // Create a simple example of a planar connected graph with vertices and edges
-    std::vector<Point> vertices = {
-        {0, 0},
-        {1, 1},
-        {1, -1},
-        {2, 0},
-        {4, 0},
-        {4, -1},
-        {-3, 0},
-        {-2, 0}
-    };
+// Coeficiente da reta que passa na origem e p.
+double inclinacao(Ponto p) {
+    return atan2(p.y, p.x);
+}
+// Coeficiente da reta orientada de p para q.
+double inclinacaoRelativa(Ponto p, Ponto q) {
+    return atan2(q.y - p.y, q.x - p.x);
+}
 
-    // Define the adjacency list for the graph
-    std::vector<std::vector<size_t>> adjacency = {
-        {2, 2, 3},
-        {4, 1, 4, 5, 7},
-        {5, 1, 4, 5, 6, 7},
-        {2, 2, 3},
-        {3, 2, 3, 6},
-        {2, 3, 5},
-        {3, 2, 3, 8},
-        {1, 7}
-    };
+// Determina se ao caminhar de a para b e depois de b para c estamos fazendo uma curva `a esquerda, `a direita, ou seguindo em frente.
+int TipoCurva(Ponto a, Ponto b, Ponto c) {
+    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
+    if (v < 0) return -1; // esquerda.
+    if (v > 0) return +1; // direita.
+    return 0; // em frente.
+}
 
-    // Call the find_faces function to find the faces of the graph
-    std::vector<std::vector<size_t>> faces = find_faces(vertices, adjacency);
-
-    // Print the faces
-    std::cout << "Faces of the graph:" << std::endl;
-    for (size_t i = 0; i < faces.size(); ++i) {
-        std::cout << "Face " << i + 1 << ": ";
-        for (size_t j = 0; j < faces[i].size(); ++j) {
-            std::cout << faces[i][j] << " ";
+std::vector<Vertice> ordenarPolar(Vertice vertice){
+    for(int k = 0; k < vertice.lista_adj.size() - 1; k++){
+        for(int i = 0; i < vertice.lista_adj.size() - 1; i++){
+            if(inclinacao(vertice.lista_adj[i].cord) > inclinacao(vertice.lista_adj[i + 1].cord)){
+                Vertice temp = vertice.lista_adj[i];
+                vertice.lista_adj[i] = vertice.lista_adj[i+1];
+                vertice.lista_adj[i+1] = temp;
+            }
         }
-        std::cout << std::endl;
     }
+
+    return vertice.lista_adj;
+}
+
+int main(){
+    Vertice v(0, 1, 1, 1);
+    v.lista_adj = {Vertice(1, 2, 3, 2), Vertice(2, 1, 5, 1)};
+    std::vector<Vertice> ordenados = ordenarPolar(v);
+
+    for(int i = 0; i < ordenados.size(); i++){
+        std::cout << ordenados[i].id << ' ';
+    }
+
+
 
     return 0;
 }
--- tp.cpp
+++ tp.cpp
@@ -2,105 +2,29 @@
 #include <vector>
 #include <cmath>
 
-struct Ponto{
-    double x, y;
-};
-
-// Dist^ancia euclidiana de a para b.
-double distancia(Ponto a, Ponto b) {
-    double x = (a.x - b.x), y = (a.y - b.y);
-    return sqrt(x*x + y*y);
-}
-
-// Coeficiente da reta que passa na origem e p.
-double inclinacao(Ponto p) {
-    return atan2(p.y, p.x);
-}
-// Coeficiente da reta orientada de p para q.
-double inclinacaoRelativa(Ponto p, Ponto q) {
-    return atan2(q.y - p.y, q.x - p.x);
-}
-
-// Determina se ao caminhar de a para b e depois de b para c estamos fazendo uma curva `a esquerda, `a direita, ou seguindo em frente.
-int TipoCurva(Ponto a, Ponto b, Ponto c) {
-    double v = a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y);
-    if (v < 0) return -1; // esquerda.
-    if (v > 0) return +1; // direita.
-    return 0; // em frente.
-}
-
-class Vertice{
-    public:
-        int numero;
-        Ponto cord;
-        int grau;
-        std::vector<int> lista_adj;
-
-        Vertice(){
-            this->numero = 0;
-            this->cord.x = 0;
-            this->cord.y = 0;
-            this->grau = 0;
-        }
-};
-
-std::vector<Vertice> vertices;
-
-void swap(Vertice* xp, Vertice* yp){
-    Vertice temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-  
-void bubblePolarSort(std::vector<Vertice> lista_adj){
-    int i, j;
-    bool swapped;
-    for (i = 0; i < lista_adj.size() - 1; i++) {
-        swapped = false;
-        for (j = 0; j < lista_adj.size() - i - 1; j++) {
-            if (atan2(lista_adj[j].cord.x, lista_adj[j].cord.y) > atan2(lista_adj[j+1].cord.x, lista_adj[j+1].cord.y)) {
-                swap(&lista_adj[j], &lista_adj[j + 1]);
-                swapped = true;
+std::vector<int> ordenar(std::vector<int> vetor){
+    for(int k = 0; k < vetor.size() - 1; k++){
+        for(int i = 0; i < vetor.size() - 1; i++){
+            if(vetor[i] > vetor[i + 1]){
+                int temp = vetor[i];
+                vetor[i] = vetor[i+1];
+                vetor[i+1] = temp;
             }
         }
-        if (swapped == false)
-            break;
     }
+
+    return vetor;
 }
 
 
 
 int main(){
-    std::cout << "Insira n(número de vértices) e m(número de arestas)\n";
-    int n, m;
-    std::cin >> n >> m;
+    std::vector<int> nums = {5, 4, 3, 2, 1, 0};
+    std::vector<int> vetor = ordenar(nums);
 
-    int i = 0;
-    for(int i = 0; i < n; i++){
-        Vertice v;
-        v.numero = i;
-        std::cin >> v.cord.x >> v.cord.y >> v.grau;
-
-        for(int j = 0; j < v.grau; j++){
-            int w;
-            std::cin >> w;
-            v.lista_adj.push_back(w);
-        }
-
-        vertices.push_back(v);
+    for(int i = 0; i < vetor.size(); i++){
+        std::cout << vetor[i] << ' ';
     }
-
-    for(int i = 0; i < vertices.size(); i++){
-        std::cout << vertices[i].numero << '\n' << vertices[i].cord.x << ' ' << vertices[i].cord.y << ' ' << vertices[i].grau;
-
-        for(int j = 0; j < vertices[i].grau; j++){
-            std::cout << ' ' << vertices[i].lista_adj[j];
-        }
-
-        std::cout << '\n';
-    }
-
-
 
     return 0;
 }
